28fc1e3ae6d263dd5a7a8926cc5a7012f77f2f8a
kaustubhhiware <hiwarekaustubh@googlemail.com>
2017-02-26 02:12:50 +0530

    X upto speed, Y to do

--- assgn3/X.c
+++ assgn3/X.c
@@ -7,21 +7,17 @@
 #include <sys/sem.h>
 #include <sys/wait.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <sys/ipc.h>
-#define MAX 1000000
-#define QUERY 0
-#define UPDATE 1
+#include <signal.h>
+#define MAX 200
 /*
 * Assignment 3 part 2
 * 14CS30011 : Hiware Kaustubh Narendra
 */
 
 // view shm -  ipcs -m
-// remove all shared memory created by you ->
+// remove all shared things created by you ->
 // ipcs -m | grep 777 | awk '{print $2}' | xargs -n 1 ipcrm -m
 // ipcs -m | grep 777 | awk '{print $2}' | xargs -n 1 ipcrm -s
 
@@ -33,18 +29,24 @@
 #define P(s) semop(s, &pop, 1)
 #define V(s) semop(s, &vop, 1)
 
-typedef struct records_ {
+typedef struct records_
+{
     char first[20];
     char last[20];
     int roll;
-    float cg;
+    float cgpa;
 } records;
 
-key_t keyx = 30; // chosen randomly
-key_t keyu = 31;
+key_t keyx = 0;
+key_t keys = 1;
+key_t keyd = 2;
+key_t keyn = 3;
+key_t keyu = 4;
+key_t keyw = 10;
+key_t keym = 11;
 int numlines();
-
-// standardised function to check for errors, no repeatition
+void saveandexit();
+// standardised function to check for errors, no repetition
 void printerror(const char* printmsg, int argcount, int* argval)
 {
     int i;
@@ -54,13 +56,17 @@ void printerror(const char* printmsg, int argcount, int* argval)
         {
             fprintf(stderr, "+--- %s : ",printmsg );
             perror("");
+            exit(0);
         }
     }
 }
+// need to declare globally to handle Ctrl+C
+int shmx, shmstud, shmdelta, shmnum, shmusers, wrt, mutex;
 
 
 int main(int argc, char* argv[])
 {
+    signal(SIGINT, saveandexit);
     char filename[100];
     if(argc > 1)
     {
@@ -71,6 +77,50 @@ int main(int argc, char* argv[])
         printf("+--- Reference file not provided! Exitting...\n");
         exit(0);
     }
+
+    int numstudents = numlines(), i = 0;
+
+    shmx = shmget(keyx, sizeof(int), 0777|IPC_CREAT);
+    shmstud = shmget(keys, numstudents*sizeof(int), 0777|IPC_CREAT);
+    shmdelta = shmget(keyd, sizeof(int), 0777|IPC_CREAT);
+    shmnum = shmget(keyn, sizeof(int), 0777|IPC_CREAT);
+    shmusers = shmget(keyu, sizeof(int), 0777|IPC_CREAT);
+    printerror("Error in shmget", 5, (int[]){ shmx, shmstud, shmdelta, shmnum, shmusers });
+
+    wrt = semget(keyw, 1, 0777|IPC_CREAT);
+    mutex = semget(keym, 1, 0777|IPC_CREAT);
+    printerror("Error in semget", 2, (int[]){ wrt, mutex });
+
+    int e1 = semctl(wrt, 0, SETVAL, 1);
+    int e2 = semctl(mutex, 0, SETVAL, 1);
+    printerror("Error in semctl", 2, (int[]){ e1, e2 });
+
+    // Read file from data
+    FILE *fp = fopen(argv[1], "r");
+    if(fp==NULL)
+    {
+        printf("+--- records.txt not found!\n");
+        exit(0);
+    }
+    records* students = (records *) shmat(shmstud, 0, 0);
+    while(!feof(fp))
+    {
+        fscanf(fp ,"%s" ,students[i].first);
+		fscanf(fp ,"%s" ,students[i].last);
+		fscanf(fp ,"%d" ,&students[i].roll);
+		fscanf(fp ,"%f" ,&students[i].cgpa);
+		i++;
+    }
+    fclose(fp);
+    int *delta, *users, *num;
+    delta = (int *) shmat(shmdelta, 0, 0);
+    users = (int *) shmat(shmusers, 0, 0);
+    num = (int *) shmat(shmnum, 0, 0);
+    printerror("Error in shmat", 3, (int[]){ *delta, *users, *num });
+    delta[0] = 0;
+    num[0] = numstudents;
+    users[0] = 0;
+
     struct sembuf pop, vop;
     // initialize sembufs
     pop.sem_num = vop.sem_num = 0;
@@ -78,35 +128,60 @@ int main(int argc, char* argv[])
     pop.sem_op = -1 ;   // value removed from semaphore
     vop.sem_op = 1 ;    // value added to semaphore
 
-    int numstudents = numlines();
-
-    int xstart = shmget(keyx, sizeof(int), 0777|IPC_CREAT);
-    int update = semget(keyu, 1, 0777|IPC_CREAT);
-    printerror("Error in semget", 1, (int[]){ xstart });
-
-    int e1;
-    // int e2 = semctl(update, 0, SETVAL, 0);
-    printerror("Error in shmctl", 1, (int[]){ e1 });
-
-    // Read file from data
-    FILE *fp = fopen("records.txt", "r");
-    if(fp==NULL)
+    while(1)
     {
-        printf("+--- records.txt not found!\n");
-        exit(0);
+        sleep(5);
+        if(delta[0]==0)
+        {
+            continue; // no changes
+        }
+
+        P(mutex);
+        users[0] += 1;
+        if(users[0]==1)
+        {
+            P(wrt);
+        }
+        V(mutex);
+
+        int s;
+        fp = fopen(argv[1],"r+");
+        for(i = 0; i < numstudents - 1; i++)
+        {
+            s += fprintf(fp, "%s %s %d %f\n", students[i].first, students[i].last, students[i].roll, students[i].cgpa );
+        }
+        printf("+--- Records have been updated\n");
+        fclose(fp);
+        delta[0] = 0;
+
+        P(mutex);
+        users[0] -= 1;
+        if(users[0]==0)
+        {
+            V(wrt);
+        }
+        V(mutex);
+
+
     }
-    printf("#students : %d\n",numstudents );
-
-    int i;
-    printf("Enter i : ");
-    scanf("%d",&i);
-
-    // move this down
-    e1 = shmctl(xstart, IPC_RMID, 0);
-    int e2 = semctl(update, 0, IPC_RMID, 0);
-    printerror("Error in semctl(remove)", 2, (int[]){ e1, e2 });
 
 }
+
+
+void saveandexit()
+{
+    int e1 = shmctl(shmx, IPC_RMID, 0);
+    int e2 = shmctl(shmstud, IPC_RMID, 0);
+    int e3 = shmctl(shmdelta, IPC_RMID, 0);
+    int e4 = shmctl(shmnum, IPC_RMID, 0);
+    int e5 = shmctl(shmusers, IPC_RMID, 0);
+    int e6 = semctl(wrt, 0, IPC_RMID, 0);
+    int e7 = semctl(mutex, 0, IPC_RMID, 0);
+    printerror("Error in shmctl(remove)", 5, (int[]){ e1, e2, e3, e4, e5 });
+    printerror("Error in semctl(remove)", 2, (int[]){ e6, e7 });
+    exit(0);
+}
+
 
 int numlines()
 {
--- assgn3/Y.c
+++ assgn3/Y.c
@@ -7,14 +7,9 @@
 #include <sys/sem.h>
 #include <sys/wait.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <sys/ipc.h>
 #include <errno.h>
-#define QUERY 0
-#define UPDATE 1
 /*
 * Assignment 3 part 2
 * 14CS30011 : Hiware Kaustubh Narendra
@@ -28,28 +23,64 @@
 * Y must wait for X to start, only then
 */
 
-key_t keyx = 30; // agrees with key of X
-key_t keyu = 31;
+key_t keyx = 0; // agrees with key of X
+key_t keys = 1;
+key_t keyd = 2;
+key_t keyn = 3;
+key_t keyu = 4;
+key_t keyw = 10;
+key_t keym = 11;
 #define P(s) semop(s, &pop, 1)
 #define V(s) semop(s, &vop, 1)
 
+void printerror(const char* printmsg, int argcount, int* argval)
+{
+    int i;
+    for( i = 0; i < argcount; i++)
+    {
+        if(argval[i]==-1)
+        {
+            fprintf(stderr, "+--- %s : ",printmsg );
+            perror("");
+            exit(0);
+        }
+    }
+}
+
+void letxstart();
+int shmx, shmstud, shmdelta, shmnum, shmusers, wrt, mutex;
+
+
 int main(int argc, char* argv[])
 {
-
-    int didxstart = shmget(keyx, sizeof(int), 0);
-    // Wait for X to start
-    while(didxstart==-1)
-    {
-        printf("Wait for X to start\n");
-        sleep(5);
-        didxstart = shmget(keyx, sizeof(int), 0);
-    }
+    letxstart();
     printf("X started! Can support operations now\n");
 
     int opt = 0;
     char *in;
+
+    shmstud = shmget(keys, numstudents*sizeof(int), 0);
+    shmdelta = shmget(keyd, sizeof(int), 0);
+    shmnum = shmget(keyn, sizeof(int), 0);
+    shmusers = shmget(keyu, sizeof(int), 0);
+    printerror("Error in shmget", 4, (int[]){ shmstud, shmdelta, shmnum, shmusers });
+
+    int *delta, *users, *num;
+    delta = (int *) shmat(shmdelta, 0, 0);
+    users = (int *) shmat(shmusers, 0, 0);
+    num = (int *) shmat(shmnum, 0, 0);
+    printerror("Error in shmat", 3, (int[]){ *delta, *users, *num });
+
+    struct sembuf pop, vop;
+    // initialize sembufs
+    pop.sem_num = vop.sem_num = 0;
+    pop.sem_flg = vop.sem_flg = 0;
+    pop.sem_op = -1 ;   // value removed from semaphore
+    vop.sem_op = 1 ;    // value added to semaphore
+
     while(1)
     {
+        letxstart();
         printf("\n");
         printf("+--------------------------------------------------+\n");
         printf("+ Following operations are supported. What to do ? +\n");
@@ -60,13 +91,24 @@ int main(int argc, char* argv[])
 
         scanf("%s", in);
         sscanf(in, "%d", &opt);
+
+        // migrate to separate functions later
         if(opt==0)
         {
             printf("+--- You chose to query. Enter roll number : ");
+            int r;
+            scanf("%s", in);
+            sscanf(in, "%d", &r);
+
+            P(mutex);
+
+
         }
         else if(opt==1)
         {
             printf("+--- You chose to update CG. Enter roll number : ");
+
+
         }
         else
         {
@@ -75,4 +117,16 @@ int main(int argc, char* argv[])
         }
     }
 
+}
+
+
+void letxstart()
+{
+    int didxstart = shmget(keyx, sizeof(int), 0);
+    while(didxstart==-1)
+    {
+        printf("Wait for X to start\n");
+        sleep(5);
+        didxstart = shmget(keyx, sizeof(int), 0);
+    }
 }
