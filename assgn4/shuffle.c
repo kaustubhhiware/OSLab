d94668ab4c91906ded0a68979c65b592eb748f33
kaustubhhiware <hiwarekaustubh@googlemail.com>
2017-03-05 14:56:59 +0530

    It works, it ain't stupid

--- assgn4/shuffle.c
+++ assgn4/shuffle.c
@@ -11,14 +11,14 @@
 // run with -
 //  gcc -pthread shuffle.c
 //
-int n, rowshift=1;// roswhift maintains if rowshift or colshift
+int rounds = 0, threads = 0;
+int n, k, x, rowshift=1;// roswhift maintains if rowshift or colshift
 int a[MAX][MAX], last[MAX], last2[MAX];
 pthread_t thread_id[MAX];
 pthread_mutex_t mutex1;
+pthread_cond_t condition;
 
 typedef struct thread_data_ {
-    int n;
-    int x;
     int index; // which thread is this
 }thread_data;
 
@@ -54,56 +54,60 @@ void *start_routine(void *param)
     int i, j;
     thread_data *t_param = (thread_data *) param;
     int tid = (*t_param).index;
-    int tx = (*t_param).x;
-    int tn = (*t_param).n;
 
-    // inside critical section
-    pthread_mutex_lock(&mutex1);
-    if(rowshift==1) // rowshift
-    {
-        for ( i = 0; i < tn; i++)
+    // while(rounds < 2*k)
+    // {
+        // inside critical section
+        pthread_mutex_lock(&mutex1);
+        if(rounds%2==0) // rowshift
         {
-            for(j = (tn/tx)*tid; j < (tn/tx)*(tid + 1) && j < n; j++)
+            for ( i = 0; i < n; i++)
             {
-                a[i][(tn+j-1)%tn] = a[i][j];
+                if(tid==0) last[i] = a[i][n-1];
+                for(j = (n/x)*tid ; j < (n/x)*(tid + 1) && j < n; j++)
+                {
+                    a[i][(n+j-1)%n] = a[i][j];
+                }
+            }
+            if(tid==x-1)
+            {
+                for(i = 0; i < n; i++)
+                {
+                    a[i][n-2] = last[i];
+                }
             }
         }
-        if(tid==tx-1)
+        else // this is columnshift
         {
-            for(i = 0; i < n; i++)
+            for ( j = 0; j < n; j++)
             {
-                a[i][n-2] = last[i];
+                if(tid==0) last2[j] = a[n-1][j];
+                for(i = n - (n/x)*tid ; i > n - (n/x)*(tid + 1) && i > 0; i--)
+                {
+                    a[i][j] = a[(n+i-1)%n][j];
+                }
+            }
+            if(tid==x-1)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    a[0][j] = last2[j];
+                }
             }
         }
-    }
-    else // this is columnshift
-    {
-        for ( j = 0; j < tn; j++)
-        {
-            for(i = tn - (tn/tx)*tid ; i > tn - (tn/tx)*(tid + 1) && i > 0; i--)
-            {
-                a[i][j] = a[(tn+i-1)%tn][j];
-            }
-        }
-        if(tid==tx-1)
-        {
-            for(j = 0; j < n; j++)
-            {
-                a[0][j] = last2[j];
-            }
-        }
-    }
-    pthread_mutex_unlock(&mutex1);
-    // Print success and exit. Actually should not print
-    // thread id with %ld, but ok here as the types match in Linux
-    printf("Thread  %ld finished updating\n", thread_id[tid]);
+
+        threads++;
+        pthread_mutex_unlock(&mutex1);
+    // }
+
+    printf("\nThread  %ld finished updating\n", thread_id[tid]);
     pthread_exit(NULL);
 }
 
 
 int main(int argc, char* argv[])
 {
-    int i, j, k, x;
+    int i, j;
     printf("+--- Enter n : ");
     scanf("%d",&n );
     printf("+--- Enter %d*%d array\n",n,n);
@@ -116,26 +120,22 @@ int main(int argc, char* argv[])
     }
     printArr();
 
-    printf("+--- Enter k and x : ");
+    printf("+--- Enter k and x : "); // Assuming x < n is given
     scanf("%d%d",&k,&x);
-
-    rowshift = 0; // first complete rowshift
-    // initialize mutex
-    pthread_mutex_init(&mutex1, NULL);
-    thread_data param[x];
-
-
-    // creating the threads - one rotation
-    for(i = 0; i < n; i++)
+    if(x>n || x<0 || k < 0)
     {
-        last[i] = a[i][n-1];
-        last2[i] = a[n-1][i];
+        printf("+--- Incorrect parameters! Exitting\n");
+        exit(0);
     }
 
+    // initialize mutex and condition
+    pthread_mutex_init(&mutex1, NULL);
+    pthread_cond_init(&condition, NULL);
+    thread_data param[x];
+
+    // creating the threads - one rotation
     for (i = 0; i < x; i++)
     {
-        param[i].n = n;
-        param[i].x = x;
         param[i].index = i;
         pthread_create(&thread_id[i], NULL, start_routine, (void *) &param[i]);
     }
@@ -147,11 +147,11 @@ int main(int argc, char* argv[])
     }
 
     pthread_mutex_lock(&mutex1);
-    printf("+--- After one rowshift\n");
+    printf("+--- After %d shifts\n",k);
     printArr();
     pthread_mutex_unlock(&mutex1);
 
     // Clean up the mutex
     pthread_mutex_destroy(&mutex1);
-
+    pthread_cond_destroy(&condition);
 }
